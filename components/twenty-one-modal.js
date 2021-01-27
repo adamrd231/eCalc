668d77a3c8fb14c221cfefc3236b2f167eabdf9e
adamrd231 <adam.rdconcepts@gmail.com>
2021-01-27 11:05:58 -0800

    Finish IAP, successful purchase on google play store.

--- components/twenty-one-modal.js
+++ components/twenty-one-modal.js
@@ -1,3 +1,4 @@
+import { firebase } from '@react-native-firebase/admob';
 import React, { useState, useEffect } from 'react';
 import { StyleSheet, View, Text, Keyboard, Button, TextInput, Modal, TouchableHighlight, Platform } from 'react-native';
 import * as RNIap from 'react-native-iap';
@@ -5,44 +6,79 @@ import {
   purchaseErrorListener,
   purchaseUpdatedListener,
   type ProductPurchase,
-  type PurchaseError
+  type PurchaseError,
 } from 'react-native-iap';
 
-const TwentyOneModal = () => {
+const TwentyOneModal = (props) => {
 
-  let purchaseUpdateProduct = null;
-  let purchaseErrorProduct = null;
+  const [ productReceipt, SetProductReceipt ] = useState(null);
+  const [ ageModalVisible, setAgeModalVisible ] = useState(true);
 
   const itemSkus = Platform.select({
     ios: ['hideAdvertising'],
     android: ['remove_ads'],
   });
 
-  const [product, setProducts ] = useState();
+
 
   useEffect(() => {
-    CetInAppPurchases()
+    console.log("Console Log product receipt", productReceipt)
+    CetAvailableInAppPurchases()
     
   }, []);
 
-  const requestPurchase = React.useCallback(async () => {
-    console.log("requestProduct for", itemSkus)
-    try {
-      await RNIap.requestSubscription(itemSkus[0], false);
-    } catch (err) {
-      console.warn(err.code, err.message)
-    }
 
-  }, []);
+  async function requestPurchase() {
+    console.log("Requesting Purchase")
+    // Start the purchase by requesting the item from the google play store
+    const purchase = await RNIap.requestPurchase('remove_ads').then( inAppPurchase => {
 
-  const CetInAppPurchases = async () => {
+      const receipt = inAppPurchase.transactionReceipt
 
+      if (receipt) {
+        // Save the product product reciept to a state variable
+        SetProductReceipt(receipt)
+        console.log("Receipt:" + receipt)
+        console.log("State Receipt" + productReceipt)
+
+        // If the product receipt is saved from the transaction then...
+        if (receipt) {
+          // Make sure we are using android then...
+          if (Platform.OS === 'android') {
+            // This is a one time purchase
+            console.log("Attempting to acknowledge purchase")
+            RNIap.acknowledgePurchaseAndroid(inAppPurchase.purchaseToken)
+          }
+
+          RNIap.finishTransaction(inAppPurchase, false)
+        } 
+      }
+    });
+  };
+
+  const CetAvailableInAppPurchases = async () => {
+
+      // Check for any pending purchases and flush them?
+      RNIap.initConnection().then(() => {
+        RNIap.flushFailedPurchasesCachedAsPendingAndroid().catch( (error) => {
+          console.log(error)
+        })
+      })
+      
+
+      // Get all available products for purchase
       const products = await RNIap.getProducts(itemSkus)
       console.log(products)
 
+      // Check purchase history, if already bought, set state.
+      const purchaseHistory = await RNIap.getPurchaseHistory()
+      console.log("Purchase History", purchaseHistory)
+      if (purchaseHistory) {
+        SetProductReceipt(purchaseHistory)
+      }
   }
 
-    const [ageModalVisible, setAgeModalVisible] = useState(true);
+   
 
     return (
         <Modal
@@ -59,25 +95,30 @@ const TwentyOneModal = () => {
               style={styles.modalButton}
               onPress={() => {
                 setAgeModalVisible(false);
+                props.SetHomeProductReceipt(productReceipt)
               }}>
               <Text style={{color: '#fff', paddingLeft: 15, paddingRight: 15}}>I AM 21 +</Text>
             </TouchableHighlight>
           </View>
           
           <View>
-            <Text>Would you like to remove video ads?</Text>
+            
+            <Text style={styles.videoTitle}>{productReceipt ? "Thanks for supporting rdConcepts" : "Would you like to remove video ads?" }</Text>
             <TouchableHighlight
-              style={styles.modalButton}
+              style={ productReceipt ? styles.modalButtonDisabled : styles.modalButton }
               onPress={() => {
                 requestPurchase()
-              }}>
+              }}
+              disabled={productReceipt}>
               <Text style={{color: '#fff', paddingLeft: 15, paddingRight: 15}}>Remove Video Ads</Text>
             </TouchableHighlight>
+            
           </View>
         </View>
         
       </Modal>
     );
+
 };
 
 const styles = StyleSheet.create({ 
@@ -101,15 +142,29 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
 
+  modalButtonDisabled: {
+    color: '#fff',
+    padding: 9,
+    backgroundColor: '#fff',
+    borderRadius: 6,
+    marginTop: 15,
+    alignItems: 'center',
+  },
+
   modalTitle: {
     fontSize: 25,
     fontWeight: 'bold',
-    marginBottom: 9,
+    textAlign: 'center',
   },
 
-  modalText: {
- 
+  videoTitle: {
+    fontSize: 15,
+    fontWeight: 'bold',
+    marginTop: 12,
+    textAlign: 'center',
+    color: "#555",
   },
+
 });
 
 export default TwentyOneModal;
